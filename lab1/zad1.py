d8a7d5f0eaa88844caaa55de6513575c726fdc8d
student189 <student189@autolab2.local>
2019-03-06 14:05:15 +0100

    dokoncz lab1

---
+++ lab1/zad1.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 13:14:06 2019
+
+@author: student189
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.datasets import load_iris
+# load the famous iris data
+irisRaw = load_iris()
+ 
+#Konwersja danych do pakietu Panda
+# read iris.data into a pandas DataFrame (df), including column names
+iris = pd.DataFrame(data= np.c_[irisRaw['data'], irisRaw['target']],
+                     columns= irisRaw['feature_names'] + ['target'])
+
+
+print(iris)
+print("______________________________________________________")
+
+print("kolumny: ", iris.shape[1])
+print("wiersze: ", iris.shape[0])
+print("______________________________________________________")
+print("opis")
+print(iris.describe())
+
+print("Grupowanie")
+print("______________________________________________________")
+print(iris.groupby('target').count())
+
+print("______________________________________________________")
+print("Piersze 5 elementów")
+print(iris.head(5))
+
+print("______________________________________________________")
+print("Brakuje danych?", iris.shape[0] - iris.dropna(how = 'any').shape[0])
+
+print("______________________________________________________")
+print("Sortowanie po 2 kolumnie")
+iris2 = iris.sort_index(by = 'sepal width (cm)')
+print(iris2)
+
+print("______________________________________________________")
+print("Min = " + str(iris[iris.columns[2]].min()) + " o adresie = " + str(iris[iris.columns[2]].idxmin()))
+print("Max = " + str(iris[iris.columns[2]].max()) + " o adresie = " + str(iris[iris.columns[2]].idxmax()))
+
+print("______________________________________________________")
+print("Odchylenie:")
+for i in iris.columns:
+    print("Kolumna",i, "\t\todchylenie = "+  str(iris[i].std()))
